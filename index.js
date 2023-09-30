6cb1113035c82a06eeaa5fbc73ddf6c516043ba2
Rachel Hamby <tnthreat33@gmail.com>
2023-09-30 10:09:03 -0400

    commit tree c8a2837ca441b311494ebbaf56098efe7fb26258

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function countDown (num){
+while (num >= 0){
+console.log(num);
+num -=1;}
+
+}
+
+function writeCards (names){
+    let i = 0
+while (i < names.length){
+    console.log(`Thank you ${names[i]}`)
+    i ++
+}
+}
